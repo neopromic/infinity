8371e1eee43905cf927effac75e9b4e39275f6e2
neopromic <cusaowesley1@gmail.com>
2024-05-06 15:31:21 -0300

    feat: working wih the auth ui
    - Deleted @/types/Users & @/types/Generics because it's unecessary at moment.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,7 +1,12 @@
+import { Input } from "@/components/ui/input";
 import { TypographyH1 } from "@/components/ui/typography/Heading1";
 
 export default function Page() {
   return (
-    <TypographyH1 text={"Hello, world"} />
+    <main className="px-4 py-6 space-y-4">
+      <TypographyH1 text={"Entre na infinity usando uma conta."} />
+      <p className="text-muted-foreground w-full text-center text-lg font-medium">Faça o login na plataforma usando um email ou senha.</p>
+      <Input placeholder="Email" className="w-md" />
+    </main>
   );
 }
