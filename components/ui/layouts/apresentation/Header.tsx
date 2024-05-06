fc6a7dd2b09accd99a13582098fd4142e676e8c2
neopromic <cusaowesley1@gmail.com>
2024-05-06 12:38:05 -0300

    feat: working with main layouts

--- components/ui/layouts/apresentation/Header.tsx
+++ components/ui/layouts/apresentation/Header.tsx
@@ -2,7 +2,8 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Header() {
   return (
-    <header className="border-b px-4 py-2  bg-transparent backdrop-blur flex items-center sticky top-0 inset-0">
+    <header className="border-b px-4 py-2 gap-2 bg-transparent backdrop-blur flex items-center sticky top-0 inset-0">
+      {/* <p className="hex1"></p> */}
       <h1 className="text-lg font-bold flex-1 select-none">infinity</h1>
       <nav>
         <ModeToggle />
--- components/ui/layouts/apresentation/HeroSection.tsx
+++ components/ui/layouts/apresentation/HeroSection.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import {  Sparkles } from "lucide-react";
 import { Button } from "../../button";
+import Link from "next/link";
 
 export default function HeroSection() {
     return (
-        <section className="px-4 py-6">
-            <h1 className="font-bold tracking-tight leading-tight text-3xl md:text-5xl lg:leading-[1.1]">Que tal uma gigantesca aventura no mundo da calistenia?</h1>
-            <h2 className="text-lg mt-4">Prepare-se para se desafiar na calistenia com a ajuda do infinity!</h2>
+        <section className="flex flex-col items-center justify-center px-4 py-6">
+            <h1 className="font-bold tracking-tight text-center leading-tight mt-8 text-3xl md:text-5xl lg:leading-[1.1]">Força, inteligência, e trabalho duro.</h1>
+            <h2 className="text-lg mt-4 text-center text-muted-foreground">Prepare-se para se desafiar na calistenia com a ajuda do projeto infinity!</h2>
+            <Link href={"/auth/signin"}>
             <Button className="my-4 gap-2">
                 <Sparkles size={"16px"} />
                 Iniciar jornada!
             </Button>
+            </Link>
         </section>
     );
 }
