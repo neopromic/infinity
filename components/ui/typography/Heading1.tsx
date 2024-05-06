c2981407f7acf31c96b350c280196a59bc026a06
neopromic <cusaowesley1@gmail.com>
2024-05-06 12:34:52 -0300

    feat: created a new component to headings texts.

---
+++ components/ui/typography/Heading1.tsx
@@ -0,0 +1,12 @@
+/**
+ * This components is used for Heading default like in HeroSections, etc
+ * @param text is the text that this component recives.
+ * @returns a component of Heading
+ */
+export function TypographyH1({ text }: { text: String }) {
+  return (
+    <h1 className="scroll-m-20 font-bold tracking-tight text-center leading-tight mt-8 text-3xl md:text-5xl lg:leading-[1.1]">
+      {text}
+    </h1>
+  );
+}
