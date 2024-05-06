a83e1639f1077c9b519746d69ab1d6aa131d5295
neopromic <cusaowesley1@gmail.com>
2024-05-06 11:27:52 -0300

    feat: added creattek logo in header component and added a css class for the logo (hexagon)

--- components/ui/layouts/apresentation/Header.tsx
+++ components/ui/layouts/apresentation/Header.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 export default function Header() {
   return (
     <header className="border-b px-4 py-2  bg-transparent backdrop-blur flex items-center sticky top-0 inset-0">
-      <h1 className="text-lg font-medium flex-1 ">infinity</h1>
+      <h1 className="text-lg font-bold flex-1 select-none">infinity</h1>
       <nav>
         <ModeToggle />
       </nav>
