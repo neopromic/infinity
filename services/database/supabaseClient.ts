7a3c1766fbb3e3b8e24686d42ab3ead3b57d5971
neopromic <cusaowesley1@gmail.com>
2024-05-19 18:21:50 -0300

    feat: :card_file_box: add database client setup

---
+++ services/database/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY!
+);
+
+export default createClient;
