fdfcdc22506920e549629a77b9f1f503bdfe7f58
neopromic <cusaowesley1@gmail.com>
2024-05-19 18:22:30 -0300

    refactor: :coffin: removed dead code

--- app/auth/signin/layout.tsx
+++ app/auth/signin/layout.tsx
@@ -1,20 +1,10 @@
-import Header from "@/components/ui/layouts/Header";
 import { Metadata } from "next";
 
-export const metadata : Metadata = {
+export const metadata: Metadata = {
   title: "Sign in into infinity",
-  description: "Sign-in into infinity-project to try our services."
-}
+  description: "Sign-in into infinity-project to try our services.",
+};
 
-export default function Layout({
-      children,
-    }: {
-      children: React.ReactNode,
-    }) {
-      return (
-        <section>
-      
-          {children}
-        </section>
-      );
-    }
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
+}
