84c8d56767dd5dd8670cefcc864bae1dd291bbb3
neopromic <cusaowesley1@gmail.com>
2024-05-20 16:17:13 -0300

    fix: :lock: security updates in commit

--- services/database/firebase.ts
+++ services/database/firebase.ts
@@ -3,14 +3,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKsvHGvCaNxS6QnoDkrw2bi0odDe0hj0o",
-  authDomain: "infinity-prd.firebaseapp.com",
-  databaseURL: "https://infinity-prd-default-rtdb.firebaseio.com",
-  projectId: "infinity-prd",
-  storageBucket: "infinity-prd.appspot.com",
-  messagingSenderId: "323059625856",
-  appId: "1:323059625856:web:b632e4122f535028b05b65",
-  measurementId: "G-PREB2XE1CK",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG_SENDER,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEANSUREMENT_ID,
 };
 
 export const app: FirebaseApp = initializeApp(firebaseConfig);
