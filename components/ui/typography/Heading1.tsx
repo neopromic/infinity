5885e592415e772df72f8f887d68d0dd6d933f5e
neopromic <cusaowesley1@gmail.com>
2024-06-01 12:44:39 -0300

    feat: now using children!

--- components/ui/typography/Heading1.tsx
+++ components/ui/typography/Heading1.tsx
@@ -3,10 +3,10 @@
  * @param text is the text that this component recives.
  * @returns a component of Heading
  */
-export function TypographyH1({ text }: { text: String }) {
+export function TypographyH1({ children }: { children: String }) {
   return (
     <h1 className="scroll-m-20 font-bold tracking-tight text-center leading-tight mt-8 text-3xl md:text-5xl lg:leading-[1.1]">
-      {text}
+      {children}
     </h1>
   );
 }
