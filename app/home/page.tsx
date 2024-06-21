c2c4f88f6a866c8dc6176244a2d184aa9eaf6a95
MiguelMaciel32 <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-21 10:56:02 -0700

    feat: add ternary if in button to enter in platform

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { TypographyH1 } from "@/components/ui/typography/Heading1";
+import { Flame } from "lucide-react";
+import { Plus } from "lucide-react";
+import Image from "next/image";
+
 export default function Page() {
-    return (
-        <main>
-          
-        </main>
-    )
+  return (
+    <section className="px-6 py-4">
+      <header>
+        <section className="container flex justify-center items-center flex-col space-y-4">
+          <Image
+            src={"/undraw_workout_gcgu.svg"}
+            alt="Workout illustration"
+            width={0}
+            height={0}
+            className="size-4/5"
+          />
+          <TypographyH1>Que jornada nos aguarda hoje?!</TypographyH1>
+          <p className="text-muted-foreground text-center">
+            Voce pode começar a utilizar rotinas de treinos criadas pela
+            comunidade ou criar a sua propria.
+          </p>
+          <Button className="gap-2">
+            <Plus />
+            Criar minha nova rotina!
+          </Button>
+          <Separator />
+        </section>
+      </header>
+      <main>
+        <h2 className="flex">
+            <Flame size={32}/>
+            Rotinas de treino feitas pela comunidade
+        </h2>
+      </main>
+    </section>
+  );
 }
