2b71f00a23a935fbe7115847bab9dd978af88e02
MiguelMaciel32 <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-21 08:27:51 -0700

    feat: update next@latest and delete "pages/..."

--- pages/api/auth/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
-  return res.json({
-    status: 200,
-    message: "Hello, World!",
-    message_type: "All systems are normal.",
-  });
-}
