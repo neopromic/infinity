287ae64cc131ee4d5c53e4d0b6ba3fe7b9d36f4d
MiguelMaciel32 <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-21 09:32:44 -0700

    feat: just some fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,7 @@
 export default function Page() {
     return (
         <main>
-          <h1>Iniciando sua jornada</h1>
-          <p>Seja bem-vindo ao Infinity, seu planejador de teinos pessoal! </p>
+          
         </main>
     )
 }
