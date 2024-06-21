465a3ae7ad94d603e3b53e41bf89909333a25563
MiguelMaciel32 <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-21 11:39:11 -0700

    fix: z-index fixed

--- components/ui/layouts/Header.tsx
+++ components/ui/layouts/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="border-b px-4 py-2 gap-2 bg-background/80 backdrop-blur flex items-center sticky top-0 inset-0">
+    <header className="border-b px-4 py-2 gap-2 bg-background/80 backdrop-blur flex items-center sticky top-0 inset-0 z-50">
       {/* <p className="hex1"></p> */}
       <Link className="flex-1" href="/">
         <h1 className="text-lg font-bold flex-1 select-none cursor-pointer">
