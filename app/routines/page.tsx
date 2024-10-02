8f33db9da03ee5364a03b1045af4503d5fe6cc32
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-02 10:58:35 -0300

    feat: deleted unnecessary code and imports

--- app/routines/page.tsx
+++ app/routines/page.tsx
@@ -1,16 +1,10 @@
-"use client";
-
-import { useState } from "react";
 import { Badge } from "@/components/ui/badge";
-import { Card, CardContent } from "@/components/ui/card";
 import {
 	Accordion,
 	AccordionContent,
 	AccordionItem,
 	AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 type WorkoutDay = {
 	day: string;
