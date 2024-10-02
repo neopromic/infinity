c9abb87a5349507232bad44fd9976779cad95083
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-02 10:12:22 -0300

    docs: :memo: add hook doc and usage examples

--- utils/context/authContext.tsx
+++ utils/context/authContext.tsx
@@ -1,85 +1,91 @@
-
 import { auth } from "@/services/database/firebase";
 import {
-  type User as IUser,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
+	type User as IUser,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
+// biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
 type User = IUser | null | void;
 type ContextState = {
-  user: User;
-  login: (email: string, password: string) => void;
-  signUp: (email: string, password: string) => void;
-  logout: () => void;
+	user: User;
+	login: (email: string, password: string) => void;
+	signUp: (email: string, password: string) => void;
+	logout: () => void;
 };
 
 const FirebaseAuthContext = createContext<ContextState | undefined>(undefined);
 
 export const FirebaseAuthProvider = ({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) => {
-  const [user, setUser] = useState<User>(() => {
-    const userCookies = Cookies.get("user");
-    return userCookies ? JSON.parse(userCookies) : null;
-  });
+	const [user, setUser] = useState<User>(() => {
+		const userCookies = Cookies.get("user");
+		return userCookies ? JSON.parse(userCookies) : null;
+	});
 
-  const login = (email: string, password: string) => {
-    // TODO: create add cookie storage
+	const login = (email: string, password: string) => {
+		// TODO: create add cookie storage
 
-    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-      const user = userCredential.user;
-      const userToken = user.getIdToken;
-      Cookies.set("user", JSON.stringify(user));
-      Cookies.set("user_token", JSON.stringify(userToken));
-    });
-  };
+		signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+			const user = userCredential.user;
+			const userToken = user.getIdToken;
+			Cookies.set("user", JSON.stringify(user));
+			Cookies.set("user_token", JSON.stringify(userToken));
+		});
+	};
 
-  const signUp = (email: string, password: string) => {
-    createUserWithEmailAndPassword(auth, email, password).then(
-      (userCredential) => {
-        const user = userCredential.user;
-        const userToken = user.getIdToken;
+	const signUp = (email: string, password: string) => {
+		createUserWithEmailAndPassword(auth, email, password).then(
+			(userCredential) => {
+				const user = userCredential.user;
+				const userToken = user.getIdToken;
 
-        Cookies.set("user", JSON.stringify(user));
-        Cookies.set("user_token", JSON.stringify(userToken));
-      }
-    );
-  };
+				Cookies.set("user", JSON.stringify(user));
+				Cookies.set("user_token", JSON.stringify(userToken));
+			},
+		);
+	};
 
-  const logout = () => {
-    // TODO: create cookie remove and logout
+	const logout = () => {
+		// TODO: create cookie remove and logout
 
-    auth.signOut();
+		auth.signOut();
 
-    Cookies.remove("user");
-    Cookies.remove("user_token");
-  };
+		Cookies.remove("user");
+		Cookies.remove("user_token");
+	};
 
-  const value = { user, login, logout, signUp };
+	const value = { user, login, logout, signUp };
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(setUser);
-    return unsubscribe;
-  }, []);
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(setUser);
+		return unsubscribe;
+	}, []);
 
-  return (
-    <FirebaseAuthContext.Provider value={value}>
-      {children}
-    </FirebaseAuthContext.Provider>
-  );
+	return (
+		<FirebaseAuthContext.Provider value={value}>
+			{children}
+		</FirebaseAuthContext.Provider>
+	);
 };
 
+/**
+ * This hook is used to get the user data and manipulate then.
+ * @returns auth hook
+ * @example
+ * const { user } = useFirebaseAuth();
+ */
 export const useFirebaseAuth = () => {
-  const context = useContext(FirebaseAuthContext);
+	const context = useContext(FirebaseAuthContext);
 
-  if (context === undefined) {
-    throw new Error("Yo! Parece que não temos uma autenticação válida!");
-  }
+	if (context === undefined) {
+		throw new Error("Yo! Parece que não temos uma autenticação válida!");
+	}
 
-  return context;
+	return context;
 };
