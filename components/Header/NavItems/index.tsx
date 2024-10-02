cf8673f56c0b3069b9478b5987a9da32d1ae62e8
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-02 15:45:59 -0300

    feat: add avatar to go to profile page

--- components/Header/NavItems/index.tsx
+++ components/Header/NavItems/index.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { useFirebaseAuth } from "@/utils/context/authContext";
+import Link from "next/link";
 
 export const NavItems = () => {
+	const { user } = useFirebaseAuth();
+	if (!user) {
+		return;
+	}
+	const avatarURl = user.photoURL as string;
+
 	return (
-		<nav>
+		<nav className="flex items-center gap-2">
+			<Link href="/profile">
+				<Avatar>
+					<AvatarImage src={avatarURl} />
+					<AvatarFallback>{user.displayName}</AvatarFallback>
+				</Avatar>
+			</Link>
 			<ModeToggle />
 		</nav>
 	);
