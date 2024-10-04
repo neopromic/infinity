d0e29d4b8c6d71368852fbb658c43b1130f02950
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-04 00:53:58 -0300

    feat: :sparkles: add private router component in profile page

---
+++ app/profile/layout.tsx
@@ -0,0 +1,9 @@
+import PrivateRouter from "@/components/ui/layouts/PrivateRouter";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<PrivateRouter>
+			<section>{children}</section>
+		</PrivateRouter>
+	);
+}
