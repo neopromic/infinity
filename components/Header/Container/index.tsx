25685cee204a1f52c4f8f28258d202dd40c8e122
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-08 21:16:00 -0300

    feat: add Drawer to Header

--- components/Header/Container/index.tsx
+++ components/Header/Container/index.tsx
@@ -1,12 +1,16 @@
-import { LogoSection } from "../LogoSection";
-import { NavItems } from "../NavItems";
+import { Drawer } from '../Drawer'
+import { LogoSection } from '../LogoSection'
+import { NavItems } from '../NavItems'
 
 export const Container = () => {
-	return (
-		<header className="top-0 z-50 sticky inset-0 flex items-center gap-2 bg-background/80 backdrop-blur px-4 py-2 border-b">
-			{/* <p className="hex1"></p> */}
-			<LogoSection />
-			<NavItems />
-		</header>
-	);
-};
+  return (
+    <header className="top-0 z-50 sticky inset-0 flex items-center gap-2 bg-background/80 backdrop-blur px-4 py-2 border-b">
+      {/* <p className="hex1"></p> */}
+      <div className="sm:block lg:hidden">
+        <Drawer />
+      </div>
+      <LogoSection />
+      <NavItems />
+    </header>
+  )
+}
