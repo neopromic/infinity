0c5018cbb19bf4d4a4509e3a0d2ca995749ac654
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-24 12:08:34 -0300

    feat: add SEO google meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,47 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/utils/providers/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
-import { Providers } from "@/utils/providers";
-import { Header } from "@/components/Header";
+import type { Metadata } from 'next'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/utils/providers/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
+import { Providers } from '@/utils/providers'
+import { Header } from '@/components/Header'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: "Infinity App",
-	description:
-		"Infinity é um projeto desenvolvido para aqueles quais gostam do esporte calistenia.",
-};
+  title: 'Infinity App',
+  description:
+    'Infinity é um projeto desenvolvido para aqueles quais gostam do esporte calistenia.',
+  verification: {
+    google: 'BdQcQyV9Tsqw5LtifWX2erZRG-4I0qx5226M2KnhLfA',
+  },
+}
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode
 }) {
-	return (
-		<>
-			<html lang="pt-BR" suppressHydrationWarning>
-				<head />
-				<body>
-					<ThemeProvider
-						attribute="class"
-						defaultTheme="system"
-						storageKey="infinity-theme"
-						enableSystem
-						disableTransitionOnChange
-					>
-						<Providers>
-							<Header.Container />
-							{children}
-							<Toaster />
-						</Providers>
-					</ThemeProvider>
-				</body>
-			</html>
-		</>
-	);
+  return (
+    <>
+      <html lang="pt-BR" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            storageKey="infinity-theme"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Providers>
+              <Header.Container />
+              {children}
+              <Toaster />
+            </Providers>
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
 }
