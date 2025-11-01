e4b25dab132717abc5ff38e7d4af8754dcbfab3e
NeO - Wesley Souza <cusaowesley1@gmail.com>
2025-11-01 12:42:41 -0300

    refactor: fixed the erros on avatar component
    - now using radix ui to avatar

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Avatar from "@/components/ui/avatar";
 import { RainbowButton } from "@/components/ui/rainbow-button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useFirebaseAuth } from "@/utils/context/authContext";
@@ -12,100 +12,97 @@ import { deleteUser } from "@/services/database/utils/deleteUser";
 import { Input } from "@/components/ui/input";
 
 export default function ProfilePage() {
-	const { user, logout } = useFirebaseAuth();
+  const { user, logout } = useFirebaseAuth();
 
-	const [pass, setPassword] = useState("");
+  const [pass, setPassword] = useState("");
 
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	const [userData, setUserData] = useState<any>(null);
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const [userData, setUserData] = useState<any>(null);
 
-	if (!user) {
-		return (
-			<main className="flex justify-center">
-				<Skeleton className="rounded-full size-32" />
-				<Skeleton className="rounded-lg w-64 h-12" />
-			</main>
-		);
-	}
+  if (!user) {
+    return (
+      <main className="flex justify-center">
+        <Skeleton className="rounded-full size-32" />
+        <Skeleton className="rounded-lg w-64 h-12" />
+      </main>
+    );
+  }
 
-	const avatarAlt = `${user.displayName} Avatar's`;
-	const avatarURl = user?.photoURL as string;
+  const avatarAlt = `${user.displayName} Avatar's`;
+  const avatarURl = user?.photoURL as string;
 
-	const userId = user.uid;
-	const userName = user.displayName as string;
-	const userEmail = user.email as string;
+  const userId = user.uid;
+  const userName = user.displayName as string;
+  const userEmail = user.email as string;
 
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	useEffect(() => {
-		getUserData();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	const getUserData = async () => {
-		const data = await readUserData(userId);
-		setUserData(data);
-	};
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    getUserData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const getUserData = async () => {
+    const data = await readUserData(userId);
+    setUserData(data);
+  };
 
-	return (
-		<main>
-			<section className="flex flex-col items-center px-4 py-12">
-				<Avatar>
-					<AvatarImage src={avatarURl} alt={avatarAlt} />
-					<AvatarFallback>{user?.displayName}</AvatarFallback>
-				</Avatar>
-				{user?.displayName ? (
-					<h1 className="font-bold text-3xl text-center sm:text-4xl md:text-5xl xl:text-6xl/none tracking-tighter">
-						Olá, {user.displayName}!
-					</h1> // Renderiza o nome se houver
-				) : (
-					<h1 className="flex items-center font-bold text-3xl text-center sm:text-4xl md:text-5xl xl:text-6xl/none tracking-tighter">
-						{userData ? (
-							`Olá, ${userData.username}`
-						) : (
-							<Skeleton className="rounded-lg w-64 h-12" />
-						)}
-					</h1>
-				)}
-				<p>
-					Gerencie suas informações de perfil em um só lugar, como seu peso,
-					idade, etc
-				</p>
+  return (
+    <main>
+      <section className="flex flex-col items-center px-4 py-12">
+        <Avatar src={avatarURl} fallback={user.displayName as string} />
+        {user?.displayName ? (
+          <h1 className="font-bold text-3xl text-center sm:text-4xl md:text-5xl xl:text-6xl/none tracking-tighter">
+            Olá, {user.displayName}!
+          </h1> // Renderiza o nome se houver
+        ) : (
+          <h1 className="flex items-center font-bold text-3xl text-center sm:text-4xl md:text-5xl xl:text-6xl/none tracking-tighter">
+            {userData ? (
+              `Olá, ${userData.username}`
+            ) : (
+              <Skeleton className="rounded-lg w-64 h-12" />
+            )}
+          </h1>
+        )}
+        <p>
+          Gerencie suas informações de perfil em um só lugar, como seu peso,
+          idade, etc
+        </p>
 
-				<Input
-					placeholder="senha"
-					onChange={(pass) => setPassword(pass.target.value)}
-				/>
-				<RainbowButton
-					onClick={async () => {
-						try {
-							await deleteUser(pass.trim());
-							console.log("Sucesso ao deletar usuário!");
-						} catch (e) {
-							console.error(`Não foi possível deletar a conta: ${e}`);
-						}
-					}}
-				>
-					Deletar conta
-				</RainbowButton>
+        <Input
+          placeholder="senha"
+          onChange={(pass) => setPassword(pass.target.value)}
+        />
+        <RainbowButton
+          onClick={async () => {
+            try {
+              await deleteUser(pass.trim());
+              console.log("Sucesso ao deletar usuário!");
+            } catch (e) {
+              console.error(`Não foi possível deletar a conta: ${e}`);
+            }
+          }}
+        >
+          Deletar conta
+        </RainbowButton>
 
-				<br />
-				<RainbowButton onClick={async () => await logout()}>Sair</RainbowButton>
-			</section>
-			<section className="px-4">
-				<h2 className="font-medium">Dados do usuário</h2>
-				{userData ? (
-					<div className="flex flex-col">
-						<p>Nome: {userData.username}</p>
-						<p>
-							Email:{" "}
-							{auth.currentUser?.email === userData.email
-								? userData.email
-								: "Houston, we have a problem."}
-						</p>
-					</div>
-				) : (
-					<p>Carregando dados do usuário...</p>
-				)}
-			</section>
-		</main>
-	);
+        <br />
+        <RainbowButton onClick={async () => await logout()}>Sair</RainbowButton>
+      </section>
+      <section className="px-4">
+        <h2 className="font-medium">Dados do usuário</h2>
+        {userData ? (
+          <div className="flex flex-col">
+            <p>Nome: {userData.username}</p>
+            <p>
+              Email:{" "}
+              {auth.currentUser?.email === userData.email
+                ? userData.email
+                : "Houston, we have a problem."}
+            </p>
+          </div>
+        ) : (
+          <p>Carregando dados do usuário...</p>
+        )}
+      </section>
+    </main>
+  );
 }
--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -1,50 +1,24 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import { cn } from "@/lib/utils";
+import { Avatar as RadixAvatar } from "radix-ui";
 
-import { cn } from "@/lib/utils"
+type AvatarProps = React.ComponentProps<typeof RadixAvatar.Root> & {
+  className?: string;
+  src: string;
+  fallback?: string;
+};
 
-const Avatar = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className
-    )}
-    {...props}
-  />
-))
-Avatar.displayName = AvatarPrimitive.Root.displayName
-
-const AvatarImage = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Image>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image
-    ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
-    {...props}
-  />
-))
-AvatarImage.displayName = AvatarPrimitive.Image.displayName
-
-const AvatarFallback = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Fallback>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Fallback
-    ref={ref}
-    className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
-      className
-    )}
-    {...props}
-  />
-))
-AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
-
-export { Avatar, AvatarImage, AvatarFallback }
+export default (props: AvatarProps) => {
+  return (
+    <RadixAvatar.Root
+      className={cn(
+        "inline-block rounded-full overflow-hidden size-10",
+        props.className
+      )}
+    >
+      <RadixAvatar.Image src={props.src} />
+      <RadixAvatar.Fallback>{props.fallback ?? "i"}</RadixAvatar.Fallback>
+    </RadixAvatar.Root>
+  );
+};
